6385781731f42d8ca33730258688eed8d7795a82
ayankhan2021 <148139254+ayankhan2021@users.noreply.github.com>
2024-01-13 23:24:42 +0500

    Add files via upload

---
+++ display_data.h
@@ -0,0 +1,6 @@
+#include "process_data.h"
+
+#ifndef DISPLAY
+#define DISPLAYEnvironmentData yesterday, EnvironmentData today, EnvironmentData tomorrow
+void display_environment_data(EnvironmentData yesterday, EnvironmentData today, EnvironmentData tomorrow);
+#endif
